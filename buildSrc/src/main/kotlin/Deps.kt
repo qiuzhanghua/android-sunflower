539d38ccffc5b3474974a657f85c9700a0d42161
Paul Merlin <paul@gradle.com>
2018-10-01 11:04:11 +0200

    Extract dependencies versions to buildSrc
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,27 @@
+object Deps {
+
+    // Sdk and tools
+    val compileSdkVersion = 28
+    val minSdkVersion = 19
+    val targetSdkVersion = 28
+
+    // App dependencies
+    val constraintLayoutVersion = "2.0.0-alpha2"
+    val coreTestingVersion = "2.0.0"
+    val espressoVersion = "3.1.0-alpha4"
+    val glideVersion = "4.8.0"
+    val gradleVersion = "3.2.0"
+    val dokkaVersion = "0.9.17"
+    val gsonVersion = "2.8.2"
+    val junitVersion = "4.12"
+    val kotlinVersion = "1.3.0-rc-116"
+    val ktlintVersion = "0.28.0"
+    val ktxVersion = "1.0.0"
+    val lifecycleVersion = "2.0.0"
+    val navigationVersion = "1.0.0-alpha06"
+    val roomVersion = "2.0.0-rc01"
+    val runnerVersion = "1.0.1"
+    val supportLibraryVersion = "1.0.0"
+    val uiAutomatorVersion = "2.2.0-alpha4"
+    val workVersion = "1.0.0-alpha09"
+}
